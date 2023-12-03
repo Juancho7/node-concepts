32bcaee63c2424975689f4fa77bf55139be8cff1
Juancho7 <juanfertrom@gmail.com>
2023-12-03 16:22:12 -0500

    styles of navbar and sidebar components were improved

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'green-yellow': {
+          50: '#f6ffe5',
+          100: '#e8ffc7',
+          200: '#d1ff95',
+          300: '#b2fe58',
+          400: '#99f532',
+          500: '#73db07',
+          600: '#57af01',
+          700: '#428506',
+          800: '#37690b',
+          900: '#2f580f',
+          950: '#163102',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
